77b4fb339531b8f89528a3e746d7534540721df2
brennanMKE <brennan@smallsharptools.com>
2016-12-07 11:49:58 -0800

    Initial Commit

---
+++ SwiftReady/SwiftReady.h
@@ -0,0 +1,19 @@
+//
+//  SwiftReady.h
+//  SwiftReady
+//
+//  Created by Brennan Stehling on 12/7/16.
+//  Copyright © 2016 SmallSharpTools LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftReady.
+FOUNDATION_EXPORT double SwiftReadyVersionNumber;
+
+//! Project version string for SwiftReady.
+FOUNDATION_EXPORT const unsigned char SwiftReadyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftReady/PublicHeader.h>
+
+
